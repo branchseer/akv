75a53c080124c622077980b561383bfc9106cfba
patr0nus <dk4rest@gmail.com>
2022-11-23 22:18:07 +0800

    Update ci scripts

--- scripts/ci.mjs
+++ scripts/ci.mjs
@@ -3,14 +3,14 @@
 process.env.VCPKG_ROOT = process.env.VCPKG_INSTALLATION_ROOT
 cd(path.resolve(__dirname, '..'))
 
-const action = argv._[0]
+await $`vcpkg install libarchive`
 
+const action = argv._[0]
 if (action === 'style-check') {
-    await $`vcpkg install libarchive`
     await $`cargo fmt --all -- --check`
     await $`cargo clippy --all-targets -- -D warnings`
 } else if (action === 'test') {
-
+    await $`cargo test --release`
 } else {
     console.error('Unrecognized action: ', action)
     process.exit(1)
