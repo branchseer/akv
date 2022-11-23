adeddf64dba11fca985501290866298f80233f77
patr0nus <dk4rest@gmail.com>
2022-11-23 23:05:20 +0800

    Update ci script

--- scripts/ci.mjs
+++ scripts/ci.mjs
@@ -1,9 +1,17 @@
 #!/usr/bin/env zx
 
+import fs from 'node:fs'
+
 cd(path.resolve(__dirname, '..'))
 
 const action = argv._[0]
-if (action === 'style-check') {
+if (action === 'vcpkg') {
+    await $`vcpkg install libarchive`
+    if (process.platform === 'linux') {
+        await $`vcpkg install liblzma`
+    }
+    fs.writeFileSync(process.env.GITHUB_ENV, 'VCPKG_ROOT=' + process.env.VCPKG_INSTALLATION_ROOT)
+} else if (action === 'style-check') {
     await $`cargo fmt --all -- --check`
     await $`cargo clippy --all-targets -- -D warnings`
 } else if (action === 'test') {
--- scripts/vcpkg_install.mjs
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env zx
-
-process.env.VCPKG_ROOT = process.env.VCPKG_INSTALLATION_ROOT
-await $`vcpkg install libarchive`
-if (process.platform === 'linux') {
-    await $`vcpkg install liblmza`
-}
