a14aa97732267f8a2e969d6411cd4ac28031895a
patr0nus <dk4rest@gmail.com>
2022-11-23 22:30:54 +0800

    Test with valgrind on Linux

--- scripts/ci.mjs
+++ scripts/ci.mjs
@@ -10,7 +10,11 @@ if (action === 'style-check') {
     await $`cargo fmt --all -- --check`
     await $`cargo clippy --all-targets -- -D warnings`
 } else if (action === 'test') {
-    await $`cargo test --release`
+    let cargoSubcommand = ''
+    if (process.platform === 'linux') {
+        cargoSubcommand = 'valgrind'
+    }
+    await $`cargo ${cargoSubcommand} test --release`
 } else {
     console.error('Unrecognized action: ', action)
     process.exit(1)
