e6bbf5a50bb1c912ef9e035532f2f5fc74b1d768
patr0nus <dk4rest@gmail.com>
2022-11-23 23:47:18 +0800

    Update ci script

--- scripts/ci.mjs
+++ scripts/ci.mjs
@@ -6,20 +6,21 @@ cd(path.resolve(__dirname, '..'))
 
 const action = argv._[0]
 if (action === 'vcpkg') {
-    await $`vcpkg install libarchive`
-    if (process.platform === 'linux') {
-        await $`vcpkg install liblzma`
+    if (process.platform === 'win32') {
+        await $`vcpkg install libarchive:x64-windows-static-md`
+    } else {
+        await $`vcpkg install libarchive`
     }
     fs.writeFileSync(process.env.GITHUB_ENV, 'VCPKG_ROOT=' + process.env.VCPKG_INSTALLATION_ROOT)
 } else if (action === 'style-check') {
     await $`cargo fmt --all -- --check`
     await $`cargo clippy --all-targets -- -D warnings`
 } else if (action === 'test') {
-    let cargoSubcommand = ''
+    let cargo = 'cargo'
     if (process.platform === 'linux') {
-        cargoSubcommand = 'valgrind'
+        cargo = 'cargo valgrind'
     }
-    await $`cargo ${cargoSubcommand} test --release`
+    await $`${cargo} test --release`
 } else {
     console.error('Unrecognized action: ', action)
     process.exit(1)
